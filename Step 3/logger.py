c671be288ef1aa833141dc16b1b95a589c82d260
saramorrison <60265404+saramorrison@users.noreply.github.com>
2020-03-05 09:57:44 -0500

    Update logger.py

--- Step 3/logger.py
+++ Step 3/logger.py
@@ -2,12 +2,14 @@
 # FILL IN HEADER
 #################
 
+import math
+import time
 import microbit as mb
 import radio  # Needs to be imported separately
 
 # Change the channel if other microbits are interfering. (Default=7)
 radio.on()  # Turn on radio
-radio.config(channel=7, length=100)
+radio.config(channel=19, length=100)
 
 print('Program Started')
 mb.display.show(mb.Image.HAPPY)
@@ -18,16 +20,18 @@ while not mb.button_a.is_pressed():  # wait for button A to be pressed to begin 
 radio.send('start') # Send the word 'start' to start the receiver
 mb.sleep(1000)
 mb.display.show(mb.Image.HEART)  # Display Heart while logging
-
+time0 = mb.running_time()
 
 # Read and send accelerometer data repeatedly until button A is pressed again
 while not mb.button_a.is_pressed():
-    ######################################################
-    # FILL In HERE
-    # Need to collect accelerometer and time measurements
-    # Need to format into a single string
-    # Send the string over the radio
-    ######################################################
+    acc_x = (mb.accelerometer.get_x())
+    acc_y = (mb.accelerometer.get_y())
+    acc_z = (mb.accelerometer.get_z())
+    time1 = mb.running_time()
+    current_time = time1 - time0
+    message = str(current_time) + "," + str(acc_x) + "," str(acc_y) + "," str(acc_z)
+    print(message)
+
 
     radio.send(message)
     mb.sleep(10)
