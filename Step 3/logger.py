21cca05f2b2bdde4ffebbc2c790bc60aa50695f6
kennedymay <60265377+kennedymay@users.noreply.github.com>
2020-03-31 16:14:39 -0400

    final commit 3/31

--- Step 3/logger.py
+++ Step 3/logger.py
@@ -4,10 +4,13 @@
 
 import microbit as mb
 import radio  # Needs to be imported separately
+import time
+import math
+from microbit import*
 
 # Change the channel if other microbits are interfering. (Default=7)
 radio.on()  # Turn on radio
-radio.config(channel=7, length=100)
+radio.config(channel=19, length=100)
 
 print('Program Started')
 mb.display.show(mb.Image.HAPPY)
@@ -18,6 +21,7 @@ while not mb.button_a.is_pressed():  # wait for button A to be pressed to begin 
 radio.send('start') # Send the word 'start' to start the receiver
 mb.sleep(1000)
 mb.display.show(mb.Image.HEART)  # Display Heart while logging
+time0 = mb.running_time
 
 
 # Read and send accelerometer data repeatedly until button A is pressed again
@@ -28,6 +32,12 @@ while not mb.button_a.is_pressed():
     # Need to format into a single string
     # Send the string over the radio
     ######################################################
+    acc_x = mb.accelerometer.get_x()
+    acc_y = mb.accelerometer.get_y()
+    acc_z = mb.accelerometer.get_z()
+    time1 = mb.running_time
+    current_time = time1 - time0
+    message = str(current_time) + "," + str(acc_x) + "," + str(acc_y) + "," + str(acc_z)
 
     radio.send(message)
     mb.sleep(10)
--- Step 3/receiver.py
+++ Step 3/receiver.py
@@ -2,12 +2,16 @@
 # FILL IN HEADER
 #################
 
+#IMPORT STATMENTS
 import microbit as mb
 import radio  # Needs to be imported separately
+import time
+import math
+from microbit import*
 
 # Change the channel if other microbits are interfering. (Default=7)
 radio.on()  # Turn on radio
-radio.config(channel=7, length =100)
+radio.config(channel=19, length =100)
 
 print('Program Started')
 mb.display.show(mb.Image.HAPPY, delay=1000, clear=True)
@@ -19,13 +23,21 @@ while not incoming == 'start':
     incoming = radio.receive()
 print('start')
 
+fout = open("data1.csv", "w")
 
 while True:
     incoming = radio.receive() # Read from radio
 
     if incoming is not None: # message was received
         mb.display.show(mb.Image.HEART, delay=100, clear=True, wait=False)
-        
+
+        data_acc = incoming
+        data_acc = data_acc.split(',')
+        data_acc = [int(data_acc[0]), int(data_acc[1]), int(data_acc[2]), int(data_acc[3]))
+        data_acc = tuple(data_acc)
+        print(data_acc)
+        fout.write(incoming)
+
         #############################################################
         # FILL IN HERE
         # Incoming is string sent from logger
@@ -33,3 +45,6 @@ while True:
         #############################################################
 
         mb.sleep(10)
+
+
+fout.close()
